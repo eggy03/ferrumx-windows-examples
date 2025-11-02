9c2f63539a024874621fada1a82c7c666623c80d
Egg-03 <111327101+eggy03@users.noreply.github.com>
2025-11-02 20:56:40 +0530

    feat(api): add example for fetching physical memory

---
+++ version-1.3.7-examples/src/main/java/memory/Win32PhysicalMemoryExample.java
@@ -0,0 +1,27 @@
+package memory;
+
+import com.ferrumx.exceptions.ShellException;
+import com.ferrumx.system.hardware.Win32_PhysicalMemory;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
+public class Win32PhysicalMemoryExample {
+
+    public static void main(String[] args) throws IOException, ShellException, InterruptedException {
+
+        // get a List of all memory IDs
+        List<String> memoryIdList = Win32_PhysicalMemory.getTag();
+
+        // For each memoryID, fetch a map of its properties
+        for(String memoryId : memoryIdList){
+            Map<String, String> memoryMap = Win32_PhysicalMemory.getMemory(memoryId);
+            // iterate through the map and print the properties
+            for(Map.Entry<String, String> memory : memoryMap.entrySet()){
+                System.out.println(memory.getKey()+ ": "+ memory.getValue());
+            }
+            System.out.println();
+        }
+    }
+}
