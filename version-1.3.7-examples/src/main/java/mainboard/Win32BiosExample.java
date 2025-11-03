03de8187959feaf90bbf2a2d6fa1b6fce48bd30a
Egg-03 <111327101+eggy03@users.noreply.github.com>
2025-11-03 12:07:49 +0530

    refactor(api): remove unused import

--- version-1.3.7-examples/src/main/java/mainboard/Win32BiosExample.java
+++ version-1.3.7-examples/src/main/java/mainboard/Win32BiosExample.java
@@ -4,7 +4,6 @@ import com.ferrumx.exceptions.ShellException;
 import com.ferrumx.system.hardware.Win32_BIOS;
 
 import java.io.IOException;
-import java.util.Map;
 
 public class Win32BiosExample {
 
