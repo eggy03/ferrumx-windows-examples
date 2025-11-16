505bdb2fa55704c051ba9cbbbc75976e97564c22
Egg-03 <111327101+eggy03@users.noreply.github.com>
2025-11-16 08:53:39 +0530

    chore(examples): add Win32NetworkCombinedExample

--- version-3.0.0-examples/src/main/java/network/combined/Win32NetworkCombinedExample.java
+++ version-3.0.0-examples/src/main/java/network/combined/Win32NetworkCombinedExample.java
@@ -1,14 +1,30 @@
 package network.combined;
 
 import io.github.eggy03.ferrumx.windows.entity.compounded.Win32NetworkAdapterToConfiguration;
+import io.github.eggy03.ferrumx.windows.entity.network.Win32NetworkAdapter;
+import io.github.eggy03.ferrumx.windows.entity.network.Win32NetworkAdapterConfiguration;
 import io.github.eggy03.ferrumx.windows.service.compounded.Win32NetworkAdapterToConfigurationService;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.List;
 
 @Slf4j
+@SuppressWarnings("unused")
 public class Win32NetworkCombinedExample {
 
-    List<Win32NetworkAdapterToConfiguration> combinedNetwork =
-            new Win32NetworkAdapterToConfigurationService().get();
+    public static void main (String[] args){
+
+        List<Win32NetworkAdapterToConfiguration> combinedNetwork =
+                new Win32NetworkAdapterToConfigurationService().get();
+
+        // json pretty print
+        combinedNetwork.forEach(network -> log.info(network.toString()));
+
+        // access individual fields
+        combinedNetwork.forEach(network -> {
+            String deviceId = network.getDeviceId();
+            Win32NetworkAdapter adapter = network.getAdapter();
+            List<Win32NetworkAdapterConfiguration> configurationList = network.getConfigurationList();
+        });
+    }
 }
