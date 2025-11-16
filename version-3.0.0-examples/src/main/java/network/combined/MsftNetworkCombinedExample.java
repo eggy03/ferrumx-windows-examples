e8f2f6dc4cc750ea8914e66e1042044b6af4d0fa
Egg-03 <111327101+eggy03@users.noreply.github.com>
2025-11-16 08:53:54 +0530

    chore(examples): add suppress warning annotation

--- version-3.0.0-examples/src/main/java/network/combined/MsftNetworkCombinedExample.java
+++ version-3.0.0-examples/src/main/java/network/combined/MsftNetworkCombinedExample.java
@@ -11,6 +11,7 @@ import lombok.extern.slf4j.Slf4j;
 import java.util.List;
 
 @Slf4j
+@SuppressWarnings("unused")
 public class MsftNetworkCombinedExample {
 
     public static void main(String[] args){
